3380f73ca27a115c4b10bbb0c26e162f1444279c
ensfrknkc <ensfrknkc58@gmail.com>
2021-06-19 00:32:24 +0300

    loops

---
+++ For Loop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır
+            //Console.Write("lütfen bir sayi giriniz:");
+            //int sayac = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= sayac; i++)
+            //{
+            //    if(i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //ekrandan girilen sayıya kadar olan tek ve cift sayilarin toplami
+            //Console.Write("lutfen bir sayi giriniz:");
+            //int sayac = int.Parse(Console.ReadLine());
+            //int tek = 0;
+            //int cift = 0;
+            //for (int i = 1; i <= sayac; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        cift += i;
+            //    }
+            //    else
+            //    {
+            //        tek += i;
+            //    }
+            //}
+            //Console.WriteLine("Tek sayilarin toplami: " + tek);
+            //Console.WriteLine("Cift sayilarin toplami: " + cift);  
+
+
+            //break,continue
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
